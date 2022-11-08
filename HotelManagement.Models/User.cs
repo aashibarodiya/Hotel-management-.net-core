9933c723dd65278125e575df68a78853e4b5c3cb
Deepak <57728553+Deepak986786@users.noreply.github.com>
2022-11-08 14:34:35 +0530

    "change photoproperty"

--- HotelManagement.Models/User.cs
+++ HotelManagement.Models/User.cs
@@ -5,7 +5,7 @@
         {
             public string Name { get; set; }
             public string Email { get; set; }
-            public string ProfileUrl { get; set; }
+            public string ProfilePic { get; set; }
 
             public string PhoneNumber { get; set; }
             public string AadhaarId { get; set; }
